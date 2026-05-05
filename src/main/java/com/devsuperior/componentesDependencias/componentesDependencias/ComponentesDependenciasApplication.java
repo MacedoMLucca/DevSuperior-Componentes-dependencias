a83c0c669789309bd6360f4487fce7cbe6453d4a
[EXT] - LUCCA MATOS MACEDO <LUMMACEDO@brasilseg.com.br>
2026-05-04 21:05:00 -0300

    inicio do desafio

---
+++ src/main/java/com/devsuperior/componentesDependencias/componentesDependencias/ComponentesDependenciasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.componentesDependencias.componentesDependencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentesDependenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentesDependenciasApplication.class, args);
+	}
+
+}
